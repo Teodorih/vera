dd7ce0e81d8b356171123da8f4bbc0d343c19197
Teodorih <teodorih@hotmail.com>
2020-04-05 18:37:17 +0300

    migration + new composer

---
+++ database/migrations/2020_04_05_152514_create_paints_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paints', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('published_at');
+            $table->string('title');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paints');
+    }
+}
